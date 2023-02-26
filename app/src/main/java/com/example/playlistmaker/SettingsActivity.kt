d6cfac274d9b61cc48704db04a2877d2c96aaf3a
Gera-sim <g-926@ya.ru>
2023-02-26 21:14:41 +0100

    Set_page buttons

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,11 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +16,28 @@ class SettingsActivity : AppCompatActivity() {
         homeButton.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
+        }
+
+        val shareButton = findViewById<LinearLayout>(R.id.Share)
+        shareButton.setOnClickListener {
+            val androidDevelopment = getString(R.string.android_developement_course)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = Uri.parse("text/plain").toString()
+            shareIntent.putExtra(Intent.EXTRA_TEXT, androidDevelopment)
+            val chooserIntent = Intent.createChooser(shareIntent, null)
+            startActivity(chooserIntent)
+        }
+
+        val supportButton = findViewById<LinearLayout>(R.id.Support)
+        supportButton.setOnClickListener{
 
         }
+
+        val agreementButton = findViewById<LinearLayout>(R.id.Agreement)
+        agreementButton.setOnClickListener {
+
+        }
+
+
     }
 }
