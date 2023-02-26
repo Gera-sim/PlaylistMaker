9546cc5db24018e053b1bcc6bd9a5dea1a95b155
Gera-sim <g-926@ya.ru>
2023-02-26 23:14:38 +0100

    Spr9_Homework2

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -20,7 +20,7 @@ class SettingsActivity : AppCompatActivity() {
 
         val shareButton = findViewById<LinearLayout>(R.id.Share)
         shareButton.setOnClickListener {
-            val androidDevelopment = getString(R.string.android_developement_course)
+            val androidDevelopment = getString(R.string.android_development_course)
             val shareIntent = Intent(Intent.ACTION_SEND)
             shareIntent.type = Uri.parse("text/plain").toString()
             shareIntent.putExtra(Intent.EXTRA_TEXT, androidDevelopment)
@@ -30,12 +30,19 @@ class SettingsActivity : AppCompatActivity() {
 
         val supportButton = findViewById<LinearLayout>(R.id.Support)
         supportButton.setOnClickListener{
-
+            val callSupport = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.supportMail)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mailSubject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.supportMessage))
+            }
+            startActivity(callSupport)
         }
 
         val agreementButton = findViewById<LinearLayout>(R.id.Agreement)
         agreementButton.setOnClickListener {
-
+            val openPage = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.agreement)))
+            startActivity(openPage)
         }
 
 
