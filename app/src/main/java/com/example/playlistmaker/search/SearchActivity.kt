a97d27d4aaf1e0c99c409b6c367ce2eb5d3f7da7
Gera-sim <g-926@ya.ru>
2023-03-31 00:59:27 +0300

    Spr11 unknown errors

--- app/src/main/java/com/example/playlistmaker/search/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/search/SearchActivity.kt
@@ -5,22 +5,40 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.ImageView
+import android.widget.*
 import androidx.recyclerview.widget.RecyclerView
 import com.example.playlistmaker.R
 import com.example.playlistmaker.model.Track
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
     companion object {
         const val SEARCH_QUERY = "SEARCH_QUERY"
     }
 
+    enum class PlaceHolder { SEARCH_RES, NOT_FOUND, ERROR }
+
     private var searchInputQuery = ""
+
     private lateinit var searchInput: EditText
     private lateinit var clearButton: ImageView
     private lateinit var rvSearch: RecyclerView
+    private lateinit var placeholderNotFound: TextView
+    private lateinit var placeholderError: LinearLayout
+    private lateinit var errorButton: Button
+
+
+    private val baseUrl = "https://itunes.apple.com"
+    private val retrofit = Retrofit
+        .Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val api = retrofit.create(ItunesAPI::class.java)
+    private val adapter = SearchAdapter { showInfo(it) }
 
     private val searchInputTextWatcher = object : TextWatcher {
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
@@ -29,7 +47,8 @@ class SearchActivity : AppCompatActivity() {
         }
 
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-        override fun afterTextChanged(s: Editable?) {}
+        override fun afterTextChanged(s: Editable?) {
+             }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,14 +59,75 @@ class SearchActivity : AppCompatActivity() {
         searchInput = findViewById(R.id.SearchForm)
         searchInput.requestFocus()
         searchInput.addTextChangedListener(searchInputTextWatcher)
+        searchInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                search()
+            }
+            false
+        }
+
+        errorButton.setOnClickListener {      search()     }
 
         clearButton = findViewById(R.id.clear)
         clearButton.visibility = clearButtonVisibility(searchInput.text)
         clearButton.setOnClickListener { clearSearchForm() }
 
         rvSearch = findViewById(R.id.rvSearchResults)
-        rvSearch.adapter = SearchAdapter(mockTracks)
+        rvSearch.adapter = adapter
+
     }
+
+    private fun search() {
+        if (searchInputQuery.isNotEmpty()) {
+            api.search(searchInputQuery).enqueue(object : Callback<SearchResponse> {
+                override fun onResponse(
+                    call: Call<SearchResponse>,
+                    response: Response<SearchResponse>
+                ) {
+                    when (response.code()) {
+                        200 -> {
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                adapter.tracks = response.body()?.results!!
+                                showPlaceholder(PlaceHolder.NOT_FOUND)
+                            }
+                        }
+                        else -> {
+                            showPlaceholder(PlaceHolder.ERROR)
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                    showPlaceholder(PlaceHolder.ERROR)
+                }
+            })
+        }
+    }
+
+    private fun showPlaceholder(placeholder: PlaceHolder) {
+        when (placeholder) {
+            PlaceHolder.NOT_FOUND -> {
+                adapter.clearTracks()
+                placeholderError.visibility = View.GONE
+                placeholderNotFound.visibility = View.VISIBLE
+            }
+            PlaceHolder.ERROR -> {
+                adapter.clearTracks()
+                placeholderNotFound.visibility = View.GONE
+                placeholderError.visibility = View.VISIBLE
+            }
+            else -> {
+                placeholderNotFound.visibility = View.GONE
+                placeholderError.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun showInfo(track: Track) {
+        val message = "${track.trackName}"
+        Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+    }
+
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -57,7 +137,10 @@ class SearchActivity : AppCompatActivity() {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         searchInputQuery = savedInstanceState.getString(SEARCH_QUERY, "")
-        searchInput.setText(searchInputQuery)
+        if (searchInputQuery.isNotEmpty()) {
+            searchInput.setText(searchInputQuery)
+            search()
+        }
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -70,6 +153,8 @@ class SearchActivity : AppCompatActivity() {
 
     private fun clearSearchForm() {
         searchInput.setText("")
+        adapter.clearTracks()
+        showPlaceholder(PlaceHolder.SEARCH_RES)
 
         val view = this.currentFocus
         if (view != null) {
@@ -78,36 +163,5 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
-    private val mockTracks = arrayListOf(
-        Track(
-            "Smells Like Teen Spirit",
-            "Nirvana",
-            "5:01",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            "Billie Jean",
-            "Michael Jackson",
-            "4:35",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg",
-        ),
-        Track(
-            "Stayin' Alive",
-            "Bee Gees",
-            "4:10",
-            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg",
-        ),
-        Track(
-            "Whole Lotta Love",
-            "Led Zeppelin",
-            "5:33",
-            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg",
-        ),
-        Track(
-            "Sweet Child O'Mine",
-            "Guns N' Roses",
-            "5:03",
-            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg",
-        )
-    )
+
 }
--- app/src/main/java/com/example/playlistmaker/search/SearchViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/search/SearchViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker.search
 
+import android.icu.text.SimpleDateFormat //Не понятно Android или Java вариант импортить
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -8,8 +9,9 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 import com.example.playlistmaker.model.Track
+import java.util.*
 
-class SearchViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val trackName: TextView = itemView.findViewById(R.id.tvTrackName)
     private val artistName: TextView = itemView.findViewById(R.id.tvArtistName)
@@ -19,8 +21,8 @@ class SearchViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
     fun bind(track: Track) {
         trackName.text = track.trackName
         artistName.text = track.artistName
-        trackTime.text = track.trackTime
-
+        trackTime.text =
+            SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis)
 
         Glide
             .with(itemView)
