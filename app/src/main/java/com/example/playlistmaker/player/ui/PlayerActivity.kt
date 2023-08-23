7e5ab17006e2ed98d6482cbf4172b68961e7be31
Gerasim <g-926@ya.ru>
2023-08-23 23:45:11 +0300

    SPR21 правки

--- app/src/main/java/com/example/playlistmaker/player/ui/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/PlayerActivity.kt
@@ -40,7 +40,16 @@ class PlayerActivity : AppCompatActivity() {
             render(it)
         }
 
-        val track = intent.getSerializableExtra(TRACK) as Track
+      /*  новый метод getSerializableExtra(String, Class)
+        предоставляет более безопасную версию по типам.
+        Он позволяет передать класс ожидаемого объекта в качестве второго аргумента.
+
+        minApi=33
+
+       val track = intent.getSerializableExtra(TRACK, Track::class.java) as Track
+       */
+
+      val track = intent.getSerializableExtra(TRACK) as Track
 
         showTrack(track)
 
